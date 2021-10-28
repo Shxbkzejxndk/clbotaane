d3294fa71f90163bf6b9eef5a5833d27760705e2
Shxbkzejxndk <91829266+Shxbkzejxndk@users.noreply.github.com>
2021-10-28 21:01:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,14 +12,51 @@ class Script(object):
 
 
     HELP_MSG = """
-➥⭕️ CHANNELS ⭕️: @CLMRG
-    
-➥ 🏅 GROUP 🏅: @CINEMA_LOKAM_MOVIE_REQUEST_GROUP
+<i>Add me as admin in your group and start filtering :)</i>
 
-➥ 🌀Creator🌀 : @Shamilmalik_10
 
-➥ 🎯MY OWNER YOUTUBE CHANNEL🎯: [GAMINGwithSHAMIL](https://youtube.com/channel/UCMNEfWvTQXn4BEgNwBxtctA)
-"""
+<b>Basic Commands;</b>
+
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+
+
+<b>Filter Commands;</b>
+
+<code>/add name reply</code>  -  Add filter for name
+
+<code>/del name</code>  -  Delete filter
+
+<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+
+<code>/viewfilters</code>  -  List all filters in chat
+
+
+
+
+<b>Connection Commands;</b>
+
+
+<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<code>/connect</code> in groups.
+
+
+<code>/connections</code>  -  Manage your connections.
+
+
+
+<b>Extras;</b>
+
+
+/status  -  Shows current status of your bot (Auth User Only)
+
+/id  -  Shows ID information
+
+<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+
+
 
 <b>© @CLMRG</b>
 """
